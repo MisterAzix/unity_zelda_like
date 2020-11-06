575e59aa2d924df2e2927c79c16c845626f9bc2a
The-SamL <samuel.labagnere@mmibordeaux.com>
2020-11-06 16:59:34 +0100

    Skip Intro

--- Assets/Scripts/Introduction.cs
+++ Assets/Scripts/Introduction.cs
@@ -10,21 +10,28 @@ public class Introduction : MonoBehaviour
 
     public RawImage backgroundImage;
 
+    public bool skipIntro = false;
+
     // Start is called before the first frame update
+    
     void Awake()
     {
+        if(!skipIntro){
         introSound.Play();
-        StartCoroutine(StopIntro());
+            StartCoroutine(StopIntro());
+        }else{
+            backgroundImage.color = new Color(1, 1, 1, 0);
+            backgroundMusic.Play();
+        }
     }
 
     IEnumerator StopIntro(){
         yield return new WaitForSeconds(41); // 41 seconds
         backgroundMusic.Play();
-        for (float i = 1; i >= 0; i -= Time.deltaTime)
-            {
-                // set color with i as alpha
-                backgroundImage.color = new Color(1, 1, 1, i);
-                yield return null;
-            }
+        for (float i = 1; i >= 0; i -= Time.deltaTime){
+            // set color with i as alpha
+            backgroundImage.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
     }
 }
