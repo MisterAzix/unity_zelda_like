bd4257229f1fd1c8283600d74cd5d6b5094c5d6c
The-SamL <samuel.labagnere@mmibordeaux.com>
2020-11-06 16:30:47 +0100

    Menu flash

--- Assets/Scripts/Menu/menuButton.cs
+++ Assets/Scripts/Menu/menuButton.cs
@@ -2,13 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class menuButton : MonoBehaviour
 {
+    public RawImage img;
+
     void OnMouseOver(){
         if(Input.GetKeyUp(KeyCode.Mouse0)){
             Debug.Log("Play!");
-            SceneManager.LoadScene("GroundFloor");
+            StartCoroutine(Fondu());
         }
     }
+
+    IEnumerator Fondu(){
+       // StartCoroutine(PlayGame());
+        for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+    }
+
+    /*IEnumerator PlayGame(){
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("GroundFloor");
+    }*/
 }
