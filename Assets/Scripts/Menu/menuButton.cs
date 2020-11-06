ce35b9e4a64b9f2151b9cf66820defa6652264d7
The-SamL <samuel.labagnere@mmibordeaux.com>
2020-11-06 16:37:29 +0100

    Ajustements

--- Assets/Scripts/Menu/menuButton.cs
+++ Assets/Scripts/Menu/menuButton.cs
@@ -6,27 +6,10 @@ using UnityEngine.UI;
 
 public class menuButton : MonoBehaviour
 {
-    public RawImage img;
-
     void OnMouseOver(){
         if(Input.GetKeyUp(KeyCode.Mouse0)){
             Debug.Log("Play!");
-            StartCoroutine(Fondu());
+            SceneManager.LoadScene("GroundFloor");
         }
     }
-
-    IEnumerator Fondu(){
-       // StartCoroutine(PlayGame());
-        for (float i = 0; i <= 1; i += Time.deltaTime)
-            {
-                // set color with i as alpha
-                img.color = new Color(1, 1, 1, i);
-                yield return null;
-            }
-    }
-
-    /*IEnumerator PlayGame(){
-        yield return new WaitForSeconds(2);
-        SceneManager.LoadScene("GroundFloor");
-    }*/
 }
