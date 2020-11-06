5ad0f0640c89449ca54308d8e42dd5722d66d73c
The-SamL <samuel.labagnere@mmibordeaux.com>
2020-11-06 16:16:47 +0100

    Introduction ajoutée

--- Assets/Scripts/Introduction.cs
+++ Assets/Scripts/Introduction.cs
@@ -1,21 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Introduction : MonoBehaviour
 {
     public AudioSource introSound;
-    public Camera mainCam;
+    public AudioSource backgroundMusic;
+
+    public RawImage backgroundImage;
 
     // Start is called before the first frame update
     void Awake()
     {
-        mainCam.enabled = false;
         introSound.Play();
+        StartCoroutine(StopIntro());
     }
 
     IEnumerator StopIntro(){
-        yield return new WaitForSeconds(41);
-        mainCam.enabled = true;
+        yield return new WaitForSeconds(41); // 41 seconds
+        backgroundMusic.Play();
+        for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                backgroundImage.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
     }
 }
