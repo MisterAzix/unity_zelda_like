1afdbddd5c1e4f824918b278de0ea50430e0837d
The-SamL <samuel.labagnere@mmibordeaux.com>
2020-11-06 15:17:45 +0100

    Introduction script ajouté

---
+++ Assets/Scripts/Introduction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introduction : MonoBehaviour
+{
+    public AudioSource introSound;
+    public Camera mainCam;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mainCam.enabled = false;
+        introSound.Play();
+    }
+
+    IEnumerator StopIntro(){
+        yield return new WaitForSeconds(41);
+        mainCam.enabled = true;
+    }
+}
