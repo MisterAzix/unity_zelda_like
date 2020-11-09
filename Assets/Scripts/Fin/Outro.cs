ae53c18f60605ae3f88870a246892320d9ea0eae
The-SamL <samuel.labagnere@mmibordeaux.com>
2020-11-09 15:15:32 +0100

    Interactions claquées + Outro Morgue

---
+++ Assets/Scripts/Fin/Outro.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Outro : MonoBehaviour
+{
+    public AudioSource outroMusic;
+    public AudioSource backgroundMusic;
+    public RawImage img;
+    private int spaceCount;
+
+    public List<DialogPage> m_dialogWithPlayer;
+    public GameObject m_overlayToDisplay = null;
+
+    public List<DialogPage> GetDialog()
+    {
+        return m_dialogWithPlayer;
+    }
+
+    public GameObject GetOverlay()
+    {
+        return m_overlayToDisplay;
+    }
+
+    void Start(){
+        spaceCount = 0;
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            spaceCount = spaceCount + 1;
+        }
+        if(spaceCount == 3){
+            outroMusic.Play();
+        }
+        if(spaceCount == 4){
+            backgroundMusic.Stop();
+            StartCoroutine(Fade());
+        }
+    }
+
+    IEnumerator Fade(){
+        for (float i = 0; i >= 0; i += Time.deltaTime){
+            // set color with i as alpha
+            img.color = new Color(0, 0, 0, i);
+            yield return null;
+        }
+        Destroy(GetComponent<Outro>());
+    }
+}
